ae57e1267a59ba75ecddc305366103b471896bfe
Thomas Anderson <thomasanderson@Mikes-MacBook-Pro.local>
2016-07-11 18:09:48 -0400

    Tesira commands as well as other changes

---
+++ Mfrequency/EqualizerFreqGainView.swift
@@ -0,0 +1,15 @@
+//
+//  EqualizerViewController.swift
+//  Mfrequency
+//
+//  Created by Thomas Anderson on 6/28/16.
+//  Copyright © 2016 CAEN. All rights reserved.
+//
+
+import UIKit
+
+class EqualizerCommandView: UIView {
+
+    
+
+}
